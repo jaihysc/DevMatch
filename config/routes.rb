1be2c0155ca6d48c7ec5f560e636fdba4acc9ae6
Johnny Cai <jaihysc@gmail.com>
2019-04-18 07:36:37 -0400

    Added contacts table

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"# Direct to home page- using PAGES_controller
   get 'about', to: "pages#about" #Direct to about page on input of about - in this case "URL/about"
-  resources :contacts
+  resources :contacts #Resources expands into 8 different routes
 end
