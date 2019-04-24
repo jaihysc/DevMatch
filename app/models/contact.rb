d6d5568557884e99fcb406f704975d05671888c1
Johnny Cai <jaihysc@gmail.com>
2019-04-24 16:43:41 -0400

    Added form validation to contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
   #Populating the properties is unnecessary as rails has 200 IQ AND CAN DO IT BY ITSELF,
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
