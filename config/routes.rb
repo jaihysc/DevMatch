bc4c2a0a8098eb4198d70c75007997eaa2880fff
Johnny Cai <jaihysc@gmail.com>
2019-04-29 17:08:32 -0400

    Optimized routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"# Direct to home page- using PAGES_controller
   get 'about', to: "pages#about" #Direct to about page on input of about - in this case "URL/about"
-  resources :contacts #Resources expands into 8 different routes
-  get 'contact-us', to: 'contacts#new' #Custom url [customPath], to: [Controller+Page]
+  resources :contacts, only: [:create] #Resources expands into 8 different routes
+  get 'contact-us', to: 'contacts#new', as: 'new_contact' #Custom url [customPath], to: [Controller+Page]
 end
