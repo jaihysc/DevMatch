d0ce0bbb2c14388a85dc2201d6311618cb43ace3
Johnny Cai <jaihysc@gmail.com>
2019-05-16 18:36:14 -0400

    Added sign up links to home page

--- db/migrate/20190516211406_add_plan_to_user.rb
+++ db/migrate/20190516211406_add_plan_to_user.rb
@@ -1,6 +1,6 @@
 class AddPlanToUser < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :plan_id, :integer #Add new plan_id column with type integer to each entry of users row
-    #Will have 0 or 1 to correspond to the plans
+    #Will have 1 or 2 to correspond to the plans
   end
 end
