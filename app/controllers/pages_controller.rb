b39ac23211d54c2c587e7f45aadf60dc94c40f7a
Johnny Cai <jaihysc@gmail.com>
2019-05-17 22:56:16 -0400

    Added plan variables to home view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController #Inheriting from application controller - name needs to follow upcase
   def home
+    #Finds the corresponding entries in the plans database by id
+    @basic_plan = Plan.find(1)
+    @pro_plan = Plan.find(2)
   end
   def about #Name of html page needs to match method - ABOUT.html.erb
   end
