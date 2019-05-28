7a59eb6fdb0e4f0922d56d06732ba925cdc737aa
Johnny Cai <jaihysc@gmail.com>
2019-05-28 16:47:45 -0400

    Added flash notice when selecting membership plan

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,4 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_action :select_plan, only: :new
+
   def create
     #Extending devise's create action to add user id for each plan
     super do |resource|
@@ -13,6 +15,15 @@ class Users::RegistrationsController < Devise::RegistrationsController
           resource.save
         end
       end
+    end
+  end
+
+  private
+  def select_plan
+    #Redirect back to basic plan if user selects a plan other than the ones defined
+    unless (params[:plan] == '1' || params[:plan] == '2' )
+      flash[:notice] = "Please select a membership plan"
+      redirect_to new_user_registration_path(plan: 1)
     end
   end
 end
