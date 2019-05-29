ca5b9c31371be4973615fdb21b9550e2b3dd732c
Johnny Cai <jaihysc@gmail.com>
2019-05-29 17:25:38 -0400

    Added profile + user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-    belongs_to :plan #Associates each user with a SINGLE plan
+  belongs_to :plan #Associates each user with a SINGLE plan
+  has_one :profile
 
   #Get parameters from forms by whitelisting with attr_accessor
   attr_accessor :stripe_card_token
