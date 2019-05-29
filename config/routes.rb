fa45839fef46099a388a876117500c708757542e
Johnny Cai <jaihysc@gmail.com>
2019-05-29 17:35:09 -0400

    Added routes for user profiles

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#home"# Direct to home page- using PAGES_controller
   devise_for :users, controllers: { registrations: 'users/registrations' }
+  resources :users do
+    resource :profile #Watch plural and singulars based on relationship
+  end
   get 'about', to: "pages#about" #Direct to about page on input of about - in this case "URL/about"
   resources :contacts, only: [:create] #Resources expands into 8 different routes
   get 'contact-us', to: 'contacts#new', as: 'new_contact' #Custom url [customPath], to: [Controller+Page]
